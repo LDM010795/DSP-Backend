4de2ecce62b0076453dd21c49582e28ae0f16dc0
LinoDM95 <linodemarco95@gmail.com>
2025-07-10 16:52:50 +0200

    minor bug fix

--- elearning/modules/models.py
+++ elearning/modules/models.py
@@ -30,8 +30,8 @@ Version: 1.0.0
 from django.db import models
 from django.conf import settings
 from django.utils.translation import gettext_lazy as _
-from django.db.models import Exists, OuterRef
-from typing import Optional, QuerySet
+from django.db.models import Exists, OuterRef, QuerySet
+from typing import Optional
 
 
 class ModuleCategory(models.Model):
