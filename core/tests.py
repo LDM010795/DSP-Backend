78c3913ee60f82f8d9ab3af22e69f5ed47c58aba
Christian <christian-litke@datasmartpoint.com>
2025-07-24 13:42:58 +0200

    Small example test. Actual test to be added.

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+class TestTestCase(TestCase):
+    def setUp(self):
+        print("This is a setup phase! Create mock data here")
+
+    def test_feature_to_test(self):
+            print("Feature getting tested!")
+
+    def tearDown(self):
+        print("This is a teardown phase! If stuff needs to be killed, do it here")
